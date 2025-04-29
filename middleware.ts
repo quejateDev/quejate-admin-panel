a639c72bcfb8e64d42d6519ecc142ab1a7d14f3d
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-04-29 10:24:07 -0500

    refactor: comment out unused authentication logic and path handling in middleware for future implementation

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,40 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getCookie, verifyToken } from '@/lib/utils'
-import { UserRole } from '@prisma/client'
+// import { getCookie, verifyToken } from '@/lib/utils'
+// import { UserRole } from '@prisma/client'
 
 export async function middleware(request: NextRequest) {
-  const authStorage = await getCookie('auth-storage')
-  const authParsed = authStorage ? JSON.parse(authStorage) : null
+  // const authStorage = await getCookie('auth-storage')
+  // const authParsed = authStorage ? JSON.parse(authStorage) : null
 
-  const token = authParsed?.state?.token
+  // const token = authParsed?.state?.token
 
-  const path = request.nextUrl.pathname;
+  // const path = request.nextUrl.pathname;
 
-  // any multimdia extension
-  if (path.includes(".")) {
-    return NextResponse.next();
-  }
+  // // any multimdia extension
+  // if (path.includes(".")) {
+  //   return NextResponse.next();
+  // }
 
-  if (path === "/") {
-    return NextResponse.redirect(new URL('/pqr', request.url))
-  }
+  // if (path === "/") {
+  //   return NextResponse.redirect(new URL('/pqr', request.url))
+  // }
 
-  if (path !== "/login") {    
-    if (!token) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
+  // if (path !== "/login") {    
+  //   if (!token) {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
 
-    const decodedToken = await verifyToken(token)
+  //   const decodedToken = await verifyToken(token)
 
-    if (!decodedToken) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
+  //   if (!decodedToken) {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
 
-    if (decodedToken?.role === UserRole.CLIENT) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  } 
+  //   if (decodedToken?.role === UserRole.CLIENT) {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
+  // } 
 
   return NextResponse.next();
 }
@@ -52,10 +52,10 @@ export const config = {
     {
       source:
         "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
+      // missing: [
+        // { type: "header", key: "next-router-prefetch" },
+        // { type: "header", key: "purpose", value: "prefetch" },
+      // ],
     }
   ],
 };
