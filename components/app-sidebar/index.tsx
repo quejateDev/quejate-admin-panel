57498600983cef4e48423a5a2105819c68b5fbe1
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-05-17 15:46:33 -0500

    feat: add 'Categorías' menu item to AppSidebar and restructure sidebar content for better organization, including conditional rendering for SUPER_ADMIN role

--- components/app-sidebar/index.tsx
+++ components/app-sidebar/index.tsx
@@ -4,7 +4,7 @@ import {
   SidebarFooter,
   SidebarHeader,
 } from "@/components/ui/sidebar";
-import { Users, Building2, LayoutDashboard } from "lucide-react";
+import { Users, Building2, LayoutDashboard, Tag } from "lucide-react";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { UserRole } from "@prisma/client";
@@ -29,7 +29,7 @@ export default async function AppSidebar() {
 
   try {
     decoded = await verifyToken(token);
-  
+
     // if (!decoded) redirect("/login");
   } catch (error) {
     console.error("Token verification error:", error);
@@ -65,10 +65,15 @@ export default async function AppSidebar() {
       url: "/entity",
       icon: Building2,
     },
+    {
+      title: "Categorías",
+      url: "/categories",
+      icon: Tag,
+    },
   ];
 
   if (role !== UserRole.ADMIN) menuItems.push(...ADMIN_MENU_ITEMS);
-  if (role === UserRole.SUPER_ADMIN) menuItems.push(...SUPER_ADMIN_MENU_ITEMS);
+  // if (role === UserRole.SUPER_ADMIN) menuItems.push(...SUPER_ADMIN_MENU_ITEMS);
 
   return (
     <Sidebar className="sidebar">
@@ -78,23 +83,50 @@ export default async function AppSidebar() {
         <OrganizationSelector userOrganizationId={decoded?.entityId || ""} />
       </SidebarHeader>
 
-      <SidebarContent className="px-3 py-4">
-        <nav className="space-y-1">
-          {menuItems.map((item) => {
-            // const isActive = pathname === item.url;
-            const isActive = false;
-            return (
-              <Link
-                key={item.url}
-                href={item.url}
-                className={`sidebar-link ${isActive ? "active" : ""}`}
-              >
-                <item.icon className="h-5 w-5" />
-                <span>{item.title}</span>
-              </Link>
-            );
-          })}
-        </nav>
+      <SidebarContent className="px-3 py-4 flex flex-col gap-6">
+        {/* seccion de super admin */}
+        <>
+          {role === UserRole.SUPER_ADMIN && (
+            <div className="flex flex-col gap-2">
+              <h2 className="text-xs font-medium border-b border-gray-300 pb-2">
+                Super Admin
+              </h2>
+              <nav className="space-y-1">
+                {SUPER_ADMIN_MENU_ITEMS.map((item) => {
+                  return (
+                    <Link href={item.url} className="sidebar-link">
+                      <item.icon className="h-5 w-5" />
+                      <span>{item.title}</span>
+                    </Link>
+                  );
+                })}
+              </nav>
+            </div>
+          )}
+
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xs font-medium border-b border-gray-300 pb-2">
+              Gestión de la organización
+            </h2>
+
+            <nav className="space-y-1">
+              {menuItems.map((item) => {
+                // const isActive = pathname === item.url;
+                const isActive = false;
+                return (
+                  <Link
+                    key={item.url}
+                    href={item.url}
+                    className={`sidebar-link ${isActive ? "active" : ""}`}
+                  >
+                    <item.icon className="h-5 w-5" />
+                    <span>{item.title}</span>
+                  </Link>
+                );
+              })}
+            </nav>
+          </div>
+        </>
       </SidebarContent>
 
       <SidebarFooter className="px-3 py-4">
